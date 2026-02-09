84e4c3ba7d73549b84e1254653f025da09d58a77
eissmann <eissmann@users.noreply.github.com>
2026-02-09 10:18:36 +0000

    chore: Sync from development release 1.0.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,9 @@
 <?php
 
 $EM_CONF["hio_typo3_connector_wtl"] = [
-    'title' => 'hio-typo3-connector-wtl',
+    'title' => 'hio-typo3-connector-frontend',
     'description' => 'TYPO3 Connector for HISinOne',
-    'version' => '1.0.3',
+    'version' => '1.0.6',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.0',
